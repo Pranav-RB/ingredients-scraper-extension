604f2f3975a818c812c52d16113a7b996935bf3c
vainavinair <vainavinair2009558@gmail.com>
2024-09-26 01:20:32 +0530

    modify extension

--- extension/popup.js
+++ extension/popup.js
@@ -6,7 +6,7 @@ document.getElementById('open-flask').addEventListener('click', function() {
         const currentTabUrl = tabs[0].url;
 
         // Flask app URL with the current tab's URL passed as a query parameter
-        const flaskAppUrl = 'http://127.0.0.1:5000/?url=' + encodeURIComponent(currentTabUrl);
+        const flaskAppUrl = 'http://127.0.0.1:5000/get_link/?url=' + encodeURIComponent(currentTabUrl);
 
         // Open a new tab with the Flask app, passing the current tab URL as a parameter
         chrome.tabs.create({ url: flaskAppUrl });
